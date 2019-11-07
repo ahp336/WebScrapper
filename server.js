f69415373ec5f9e3c1181713dfb99d040e841b0d
ahp336 <aakashparikh8@gmail>
2019-11-07 02:57:22 -0500

    Adding a server file to the app

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 8000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/webscraper";
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Require routes
+require('./routing/routing.js')(app);
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App listening on http://localhost:" + PORT);
+});
